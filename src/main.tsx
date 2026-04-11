e05125e4dba406989c28f3445aa52d21f075819e
wccc-wisconsin <zimingwu@wisccc.org>
2026-04-11 00:03:47 -0500

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import AdminPage from './admin/AdminPage.tsx'
+import AnthemModule from './modules/anthem/AnthemModule.tsx'
 import './styles/globals.css'
+
+const path = window.location.pathname
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {path.startsWith('/admin')  ? <AdminPage />    :
+     path.startsWith('/anthem') ? <AnthemModule /> :
+     <App />}
   </React.StrictMode>,
 )
