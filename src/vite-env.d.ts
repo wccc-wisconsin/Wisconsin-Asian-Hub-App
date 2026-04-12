31759a2901251d3c0193b8479d576ff46c43d650
wccc-wisconsin <zimingwu@wisccc.org>
2026-04-12 13:11:29 -0500

    Add files via upload

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -12,6 +12,8 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_STORAGE_BUCKET: string
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
   readonly VITE_FIREBASE_APP_ID: string
+  readonly VITE_EVENTBRITE_TOKEN: string
+  readonly VITE_GOOGLE_MAPS_KEY: string
 }
 
 interface ImportMeta {
